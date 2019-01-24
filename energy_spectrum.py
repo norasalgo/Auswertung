1b6123e8fe684cb0cdd83aadfec05b059c9bc981
norasalgo <nora.salgo@uzh.ch>
2019-01-24 14:31:41 +0100

    define gaussian and ex_gaussian

--- energy_spectrum.py
+++ energy_spectrum.py
@@ -1,9 +1,28 @@
 import matplotlib.pyplot as plt
-data = []
-with open('C:\\Users\\NoraS\\Dropbox\\Dokumente\\Studium\\KT\\Praktikum\\Messdaten\\PMT1_energy_nogate - Kopie.Spe') as file:
-    for line in file.readlines():
-        data.append(line[:-1])
+import numpy as np
+import scipy as sp
+import scipy.special.erfc as erfc
+
+# ---Load Data ---------------
+
+path_data = './data_notext'
+
+data = np.loadtxt(path_data + '/Al_time_20min.spe')
+
+
+# ------Fitshit-----
+
+
+def gaussian(x,mu, sigma):
+    return 1/(np.sqrt(2*np.pi)*sigma)*np.exp(-1/2*((x-mu)/sigma)**2)
+
+
+def ex_gaussian(x, mu, sigma, lam):
+    return lam / 2 * np.exp(lam / 2 * (2 * mu + lam * sigma ** 2 - 2 * x)) * erfc(
+        (mu + lam * sigma ** 2 - x) / (np.sqrt(2) * sigma))
+
 
 x = range(len(data))
-plt.plot(x,data)
+plt.scatter(x, data)
+plt.show()
 print(data[20:40])
