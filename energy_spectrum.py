a3354d635f72cf1498feada0d39f8a4e4b325699
norasalgo <nora.salgo@uzh.ch>
2019-01-24 16:45:44 +0100

    plot den fit

--- energy_spectrum.py
+++ energy_spectrum.py
@@ -8,7 +8,8 @@ path_data = './data_notext'
 
 y_data = np.loadtxt(path_data + '/Al_time_20min.spe')
 y_data = np.array(y_data)
-x_data = np.arange(len(y_data))
+x_data = np.arange(len(y_data))[1000:]
+y_data = y_data[1000:]
 sigma_i = np.sqrt(y_data)
 
 # ------fitshit-----
@@ -22,14 +23,25 @@ def ex_gaussian(x, mu, sig, lam):
     return lam / 2 * np.exp(lam / 2 * (2 * mu + lam * sig ** 2 - 2 * x)) * erfc(
         (mu + lam * sig ** 2 - x) / (np.sqrt(2) * sig))
 
+#
+# def f_al (x, a, b, mu, sig):
+#     return a * b*gaussian(x, mu, sig)
 
-def f_al (x, a, b, mu, sig):
+def f_al (x, *params):
+    a, b, mu, sig = params
     return a * b*gaussian(x, mu, sig)
 
-def chi2():
-    pass
 
-y_fit, cov = scipy.optimize.curve_fit(f=f_al,xdata=x_data,ydata=y_data)
 
+fit_params, cov = scipy.optimize.curve_fit(f=f_al,xdata=x_data,ydata=y_data, p0=(1,1,1900,10))
+# a_fit, b_fit, mu_fit, sig_fit = fit_params
+y_fit = f_al(x_data,*fit_params)
+print(fit_params)
 
 #plotshit------------
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.scatter(x_data,y_data, marker = '.')
+ax.scatter(x_data, y_fit,marker = ".")
+fig.savefig('al_time_fit.png', dpi = 300)
+plt.show()
