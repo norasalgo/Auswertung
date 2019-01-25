c00b7de0ac242452505590f384813582e88f82e0
norasalgo <nora.salgo@uzh.ch>
2019-01-25 15:02:49 +0100

    calibration done

--- time_calibration.py
+++ time_calibration.py
@@ -11,21 +11,43 @@ path_data = './data_notext'
 
 y_data = np.loadtxt(path_data + '/timecalibration.spe')
 y_data = np.array(y_data)
+y_data = y_data/10e4
 x_data = np.arange(len(y_data))
 
-
 #peaks--------------------
+cuts = [700,1000,1300,1700,2000,2300,2650,3000,3300,3650,4000]
 
-p_1 = [900,1000]
+weighted_means = []
+for i in range(len(cuts)-1):
+    wm = sum(y_data[cuts[i]:cuts[i+1]]*x_data[cuts[i]:cuts[i+1]])/ sum(y_data[cuts[i]:cuts[i+1]])
+    weighted_means.append(wm)
 
+#Plot every peak to make sure we have the right cuts
+plt.plot(x_data,y_data)
+plt.xlabel('Channel number')
+plt.ylabel(r'Event counts $[10^4]$')
+for i in range(len(cuts)-1):
+    plt.plot(x_data[cuts[i]:cuts[i+1]], y_data[cuts[i]:cuts[i+1]])
+    plt.axvline(x=weighted_means[i], color='b', linestyle='--', linewidth = 0.5)
+plt.show()
+
+
+#calculate alpha factor (bins/ns)
+
+peak_distances = np.array([cuts[i+1]-cuts[i] for i in range(len(cuts)-1)])
+mean_distance = sum(peak_distances)/len(peak_distances)
+alpha = mean_distance/8
 
 #plot----------
 
 fig = plt.figure()
-plt.plot(x_data,y_data/10e4)
+plt.plot(x_data,y_data)
 plt.xlabel('Channel number')
 plt.ylabel(r'Event counts $[10^4]$')
 
-plt.plot(x_data[p_1[0]:p_1[1]],y_data[p_1[0]:p_1[1]],'r')
-plt.show()
+#plt.plot(x_data[p_1[0]:p_1[1]],y_data[p_1[0]:p_1[1]],'r')
+
+
+
+# plt.show()
 #fig.savefig('time_calibration_data.png', dpi = 600)
--- time_plastic.py
+++ time_plastic.py
@@ -4,6 +4,7 @@ from scipy.special import erfc as erfc
 
 import scipy.optimize
 
+
 # ---Load Data ---------------
 
 path_data = './data_notext'
@@ -18,7 +19,7 @@ for y_i in y_data:
     else:
         sigma_i.append(np.sqrt(y_i))
 
-
+alpha = 41.25 #time conversion
 # ------fitshit-----
 
 
@@ -55,40 +56,43 @@ print(fit_params_1)
 fit_params_2, cov_2 = scipy.optimize.curve_fit(f=f_plas_t_2, xdata=x_data[1000:], ydata=y_data[1000:], p0=start_param_2)
 a_fit_2, b_fit_2, c_fit_2, d_fit_2, mu_fit_2, sig_fit_2, lam_1_fit_2, lam_2_fit_2 = fit_params_2
 print(fit_params_2)
-
-# # plotshit------------
-x_start = 1600
-x_stop = 2000
-x_fit_1 = np.linspace(x_start, x_stop, 1000)
-y_fit_1 = f_plas_t_1(x_fit_1, *fit_params_1)
-
-x_fit_2 = np.linspace(x_start, x_stop, 1000)
-y_fit_2 = f_plas_t_2(x_fit_2, *fit_params_2)
-
-fig = plt.figure()
-ax_1 = plt.subplot(211)
-ax_1.set_xlabel('chanel number')
-ax_1.set_ylabel('counter')
-ax_1.scatter(x_data[x_start:x_stop], y_data[x_start:x_stop], marker='.', s=0.8, label='data')
-ax_1.plot(x_fit_1, y_fit_1, 'r', linewidth=0.5, label='fit')
-ax_1.plot(x_fit_1, b_fit * gaussian(x_fit_1, mu_fit, sig_fit), 'g--', linewidth=0.5, label='gaussian')
-ax_1.plot(x_fit_1, len(x_fit_1) * [a_fit], 'b--', linewidth=0.5, label='Background')
-ax_1.plot(x_fit_1, c_fit * ex_gaussian(x_fit_1, mu_fit, sig_fit, lam_fit), 'm--', linewidth=0.5, label='Convolution')
-
-ax_2 = plt.subplot(212)
-ax_2.set_xlabel('chanel number')
-ax_2.set_ylabel('counter')
-ax_2.scatter(x_data[x_start:x_stop], y_data[x_start:x_stop], marker='.', s=0.8, label='Data')
-ax_2.plot(x_fit_2, y_fit_2, 'r', linewidth=0.5, label='Fit (Total)')
-ax_2.plot(x_fit_2, b_fit_2 * gaussian(x_fit_2, mu_fit_2, sig_fit_2), 'g--', linewidth=0.5, label='Fit: Gaussian')
-ax_2.plot(x_fit_2, len(x_fit_2) * [a_fit_2], 'b--', linewidth=0.5, label='Fit: Background')
-ax_2.plot(x_fit_2, c_fit_2 * ex_gaussian(x_fit_2, mu_fit_2, sig_fit_2, lam_1_fit_2), 'm--', linewidth=0.5,
-          label='Fit: Convolution1')
-ax_2.plot(x_fit_2, d_fit_2 * ex_gaussian(x_fit_2, mu_fit_2, sig_fit_2, lam_2_fit_2), 'm--', linewidth=0.5,
-          label='Fit: Convolution2')
-
-
-ax_1.legend(loc=1)
-ax_2.legend(loc=1)
-plt.show()
-fig.savefig('plastic_time_fit_both.png', dpi=600)
+tau_1 = 1/(lam_1_fit_2*alpha)
+tau_2 = 1/(lam_2_fit_2*alpha)
+print(tau_1,tau_2)
+#
+# # # plotshit---------------------------------------------------------------------
+# x_start = 1600
+# x_stop = 2000
+# x_fit_1 = np.linspace(x_start, x_stop, 1000)
+# y_fit_1 = f_plas_t_1(x_fit_1, *fit_params_1)
+#
+# x_fit_2 = np.linspace(x_start, x_stop, 1000)
+# y_fit_2 = f_plas_t_2(x_fit_2, *fit_params_2)
+#
+# fig = plt.figure()
+# ax_1 = plt.subplot(211)
+# ax_1.set_xlabel('chanel number')
+# ax_1.set_ylabel('counter')
+# ax_1.scatter(x_data[x_start:x_stop], y_data[x_start:x_stop], marker='.', s=0.8, label='data')
+# ax_1.plot(x_fit_1, y_fit_1, 'r', linewidth=0.5, label='fit')
+# ax_1.plot(x_fit_1, b_fit * gaussian(x_fit_1, mu_fit, sig_fit), 'g--', linewidth=0.5, label='gaussian')
+# ax_1.plot(x_fit_1, len(x_fit_1) * [a_fit], 'b--', linewidth=0.5, label='Background')
+# ax_1.plot(x_fit_1, c_fit * ex_gaussian(x_fit_1, mu_fit, sig_fit, lam_fit), 'm--', linewidth=0.5, label='Convolution')
+#
+# ax_2 = plt.subplot(212)
+# ax_2.set_xlabel('chanel number')
+# ax_2.set_ylabel('counter')
+# ax_2.scatter(x_data[x_start:x_stop], y_data[x_start:x_stop], marker='.', s=0.8, label='Data')
+# ax_2.plot(x_fit_2, y_fit_2, 'r', linewidth=0.5, label='Fit (Total)')
+# ax_2.plot(x_fit_2, b_fit_2 * gaussian(x_fit_2, mu_fit_2, sig_fit_2), 'g--', linewidth=0.5, label='Fit: Gaussian')
+# ax_2.plot(x_fit_2, len(x_fit_2) * [a_fit_2], 'b--', linewidth=0.5, label='Fit: Background')
+# ax_2.plot(x_fit_2, c_fit_2 * ex_gaussian(x_fit_2, mu_fit_2, sig_fit_2, lam_1_fit_2), 'm--', linewidth=0.5,
+#           label='Fit: Convolution1')
+# ax_2.plot(x_fit_2, d_fit_2 * ex_gaussian(x_fit_2, mu_fit_2, sig_fit_2, lam_2_fit_2), 'm--', linewidth=0.5,
+#           label='Fit: Convolution2')
+#
+#
+# ax_1.legend(loc=1)
+# ax_2.legend(loc=1)
+# plt.show()
+# fig.savefig('plastic_time_fit_both.png', dpi=600)
