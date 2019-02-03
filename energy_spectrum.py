c653ed1497defd8d8c310a3ceca1d6a2964b7aea
norasalgo <nora.salgo@uzh.ch>
2019-02-03 12:27:04 +0100

    set up energy plots

--- energy_spectrum.py
+++ energy_spectrum.py
@@ -8,11 +8,20 @@ import scipy.optimize
 path_data = './data_notext'
 
 
-y_data_1 = np.loadtxt(path_data + '/PMT1_energy_nogate.spe')[40:]
-y_data_2 = np.loadtxt(path_data + '/PMT2_energy_nogate.spe')[40:]
-x_data = np.arange(len(y_data_1))
+y_pmt1_nogate = np.loadtxt(path_data + '/PMT1_energy_nogate.spe')[40:]
+y_pmt2_nogate = np.loadtxt(path_data + '/PMT2_energy_nogate.spe')[40:]
+y_pmt1_withgate = np.loadtxt(path_data + '/PMT1_energy1275_withgate.spe')
+y_pmt2_withgate = np.loadtxt(path_data + '/PMT2_energy511_withgate.spe')
 
+fig = plt.figure()
 
-plt.plot(x_data,y_data_2)
-plt.yscale('log')
+ax_1 = plt.subplot(221)
+ax_1.scatter(np.arange(len(y_pmt1_nogate)),y_pmt1_nogate, s=0.8)
+ax_2 = plt.subplot(222)
+ax_2.scatter(np.arange(len(y_pmt2_nogate)),y_pmt2_nogate,s=0.8)
+ax_3 = plt.subplot(223)
+ax_3.scatter(np.arange(len(y_pmt1_withgate)),y_pmt1_withgate, s=0.8)
+ax_4 = plt.subplot(224)
+ax_4.scatter(np.arange(len(y_pmt2_withgate)),y_pmt2_withgate, s= 0.8)
+# plt.yscale('log')
 plt.show()
